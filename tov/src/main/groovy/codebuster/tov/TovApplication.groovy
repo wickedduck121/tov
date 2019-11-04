28b7e9c7edcfe055dc0c231df7efce6d898c9d61
user <dezer121@gmail.com>
2019-11-04 17:24:06 +0200

    create

---
+++ tov/src/main/groovy/codebuster/tov/TovApplication.groovy
@@ -0,0 +1,13 @@
+package codebuster.tov
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TovApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TovApplication, args)
+	}
+
+}
